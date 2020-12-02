c13819c04e81c5071bf27e542bb9d1f1928df0d3
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2020-12-01 23:19:22 -0700

    Update

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-// server.js
-// where your node app starts
 
-// init project
+
+
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200})); 
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
 
 function timeConverter(UNIX_timestamp, format){
   var a = new Date(UNIX_timestamp * 1000);
@@ -57,10 +51,6 @@ app.get("/api/timestamp", (req, res) => {
   res.json({ "unix": today.getTime(), "utc": today.toUTCString() })
 }) 
 
-
-
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
